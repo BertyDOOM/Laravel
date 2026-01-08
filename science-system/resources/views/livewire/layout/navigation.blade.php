092f3267ba011df1c1e4d4a34d644153c96879b2
BertyDOOM <165205738+BertyDOOM@users.noreply.github.com>
2026-01-08 15:15:10 +0000

    Added Publications to <nav>

--- science-system/resources/views/livewire/layout/navigation.blade.php
+++ science-system/resources/views/livewire/layout/navigation.blade.php
@@ -33,6 +33,9 @@ new class extends Component
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('publications.index')" :active="request()->routeIs('publications.*')">
+                       {{ __('Publications') }}
+                    </x-nav-link>
                 </div>
             </div>
 
